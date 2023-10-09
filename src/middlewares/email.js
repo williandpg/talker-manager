ba8db42f6861e51dc4f472e703ee3eb9a0090355
Willian Gonçalves <willian.p.g@outlook.com>
2023-10-09 19:06:39 -0300

    Requisito 04 - Adicione as validações para o endpoint /login

---
+++ src/middlewares/email.js
@@ -0,0 +1,17 @@
+function emailValid(email) {
+  const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.([a-zA-Z]{2,4})+$/;
+  return regex.test(email);
+}
+
+function emailInput(req, res, next) {
+  const { email } = req.body;
+  if (!email || email === '') {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+  if (!emailValid(email)) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  next();
+}
+
+module.exports = emailInput;
--- src/routes/login.js
+++ src/routes/login.js
@@ -1,7 +1,9 @@
 const login = require('express').Router();
+const { emailInput } = require('../middlewares/email');
+const { passwordInput } = require('../middlewares/password');
 const { generateToken } = require('../utils/utils');
 
-login.post('/login', async (req, res) => {
+login.post('/login', emailInput, passwordInput, async (req, res) => {
   const { email, password } = req.body;
   if (email === '' || password === '') {
     return res.status(401).json({ message: 'Todos os campos devem ser preenchidos' });
