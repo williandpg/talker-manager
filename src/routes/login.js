8aaf9b339f19d73f60a245484b207d8ed5d03cf5
Willian Gonçalves <willian.p.g@outlook.com>
2023-10-12 18:00:04 -0300

    Requisito 04 - Adicione as validações para o endpoint /login

--- src/routes/login.js
+++ src/routes/login.js
@@ -3,10 +3,6 @@ const { emailInput, passwordInput } = require('../middlewares');
 const { generateToken } = require('../utils/utils');
 
 login.post('/login', emailInput, passwordInput, async (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    return res.status(401).json({ message: 'Todos os campos devem ser preenchidos' });
-  }
   const token = generateToken();
   return res.status(200).json({ token });
 });
